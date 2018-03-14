8d4c8f24a939376af5c6648fe6b44cfa19cfd75f
Khadim GNINGUE <khadimracky92@gmail.com>
2018-03-14 20:44:59 +0100

    Ajout des fichiers Employe et Etudiant

---
+++ Employe.java
@@ -0,0 +1,12 @@
+public class Employe{
+	 //Proprités
+	 private String nom;
+	 private String prenom;
+	 //Constructeurs
+	 public Employe(){
+	 }
+	 public Employe(String nom, String prenom){
+		this.nom = nom;
+		this.prenom = prenom;
+	 }
+}
---
+++ Etudiant.java
@@ -0,0 +1,12 @@
+public class Etudiant{
+	 //Proprités
+	 private String nomEtudiant;
+	 private String prenomEtudiant;
+	 //Constructeurs
+	 public Etudiant(){
+	 }
+	 public Etudiant(String nomEtudiant, String prenomEtudiant){
+		this.nomEtudiant = nomEtudiant;
+		this.prenomEtudiant = prenomEtudiant;
+	 }
+}
